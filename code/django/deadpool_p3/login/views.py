2024fbf4dbdb2bafbffc6515f5a3bbd6a0bce98d
Thomas Shaw <thomas@utulsa.edu>
2017-11-06 09:29:47 -0600

    Apparently these changes didn't get commited last night?

--- code/django/deadpool_p3/login/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, render_to_response
-from django.http import HttpResponse
-
-def index(request):
-    # return HttpResponse("<h2>Ole!</h2>")
-    return render(request, 'login/test.html')
